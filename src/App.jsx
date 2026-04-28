e571ee8824ab5feba977808f3237aa4d521a9dea
AditiKesharwani <hiaditii2001@gmail.com>
2026-04-28 11:43:23 +0530

    Practiced about useRef using ref.current property

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import UseRef1 from './UseRef1'
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <UseRef1/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/UseRef1.jsx
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+
+export default function UseRef1(){
+    let ref =useRef(0)
+    function handleClick (){
+        ref.current = ref.current+1
+        alert('you clicked' + ref.current + 'times!')
+    }
+
+    return (
+        <button onClick={handleClick}>Click me</button>
+    )
+}
