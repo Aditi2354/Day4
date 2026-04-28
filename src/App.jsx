e365f8854144c2a34ad9954b3e6f98be380c5624
AditiKesharwani <hiaditii2001@gmail.com>
2026-04-28 13:30:50 +0530

    Practiced useMemo with example

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import UseRef1 from './UseRef1'
 import DOMvalue from './Useref2'
+import HeavyCalculation from './UseMemo'
 function App() {
   
 
@@ -16,6 +17,9 @@ function App() {
       <div>
         <DOMvalue/>
       </div>
+      <>
+      <HeavyCalculation/>
+      </>
     </>
   )
 }
